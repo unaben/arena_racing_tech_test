4b39ec42c754e569a10e70e4f408b72dd01a1ecc
unaben <unaben@yahoo.com>
2023-03-27 12:29:27 +0100

    [master] add dotenv file and hide token

--- src/components/hook/useFetch.ts
+++ src/components/hook/useFetch.ts
@@ -3,12 +3,13 @@ import { IRacingData } from "../model/localization";
 // import axios from "axios";
 
 const useFetch = (enteredMonthNumber: string) => {
-  const [eventData, setEventData] = useState<IRacingData[]>([]); 
+  const [eventData, setEventData] = useState<IRacingData[]>([]);
 
   const fetchData = async () => {
-    const baseUrl = `https://api.arenaracingcompany.co.uk/event/month/1318/${enteredMonthNumber ? parseInt(enteredMonthNumber) : 1}`;    
-    const token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUkMiLCJhdWQiOiIzIiwiZXhwIjoxNjc5OTA3NTM3fQ.mP81lyjEzOPcxlKuYubwVVea0cYPRlSu58k0POdL6IY";
+    const baseUrl = `https://api.arenaracingcompany.co.uk/event/month/1318/${
+      enteredMonthNumber ? parseInt(enteredMonthNumber) : 1
+    }`;
+    const token = process.env.REACT_APP_BEARER_TOKEN;
     // const URL = "http://localhost:3040/eventData";
 
     try {
@@ -22,7 +23,7 @@ const useFetch = (enteredMonthNumber: string) => {
       if (!res.ok) {
         throw new Error(`Request failed: ${res.status}`);
       }
-      const resData = await res.json();      
+      const resData = await res.json();
       setEventData(resData);
     } catch (error) {
       if (error instanceof Error) {
